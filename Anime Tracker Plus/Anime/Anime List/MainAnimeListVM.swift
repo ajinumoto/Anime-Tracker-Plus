7ce616d695761db329763b99b3348c66bdd562b9
Adjie Satryo P <adjie.pamungkas@erajaya.com>
2024-10-29 09:42:43 +0700

    Initial Reales

---
+++ Anime Tracker Plus/Anime/Anime List/MainAnimeListVM.swift
@@ -0,0 +1,8 @@
+//
+//  MainAnimeListVM.swift
+//  Anime Tracker Plus
+//
+//  Created by Adjie Satryo on 29/10/24.
+//
+
+import Foundation
---
+++ Anime Tracker Plus/Anime/Anime List/MainAnimeListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainAnimeListView.swift
+//  Anime Tracker Plus
+//
+//  Created by Adjie Satryo on 29/10/24.
+//
+
+import SwiftUI
+
+struct MainAnimeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainAnimeListView()
+}
